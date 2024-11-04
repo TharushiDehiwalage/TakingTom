51c880902054cb949bc8a43651404aa5672d620b
NVQ 5 <dtrdehiwalage@gmail.com>
2024-11-04 09:58:35 +0530

    taking tom welcome

---
+++ app/src/main/java/com/example/takingtom/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.takingtom
+
+import android.content.Intent
+import android.os.*
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent=Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
+
+    }
+}
